cea18a8d8e11ee9343159b05de43501d7b9f823b
asix7 <andres_cr723@hotmail.com>
2015-04-05 19:22:44 +1000

    Change of tracks demo
    Simulation with new Json data

--- home/home.js
+++ home/home.js
@@ -1,28 +1,4 @@
 
-$(document).ready(function(){
-
-	//Rotate the refresh icon once 
-	$('.new-songs').click(function(){
-    	var interval = null;
-    	var counter = 0;
-    	var $this = $('.refresh');
-    	clearInterval(interval);
- 
-   		interval = setInterval(function(){
-        	if (counter != -360) {
-            	counter -= 5;
-           	 	$this.css({
-            	    MozTransform: 'rotate(-' + -counter + 'deg)',
-                	WebkitTransform: 'rotate(' + -counter + 'deg)',
-                	transform: 'rotate(' + -counter + 'deg)'
-            	});
-        	}
-    	}, 10);
-	});	
-
-});
-
-
 //Test for the grind
 var tracks = '{"tracks":[' +
 '{"cover":"common/1.jpg"},' +
@@ -50,6 +26,32 @@ var tracks = '{"tracks":[' +
 '{"cover":"common/23.jpg"},' +
 '{"cover":"common/24.jpg"}]}';
 
+var new_tracks= '{"tracks":[' +
+'{"cover":"common/2.jpg"},' +
+'{"cover":"common/12.jpg"},' +
+'{"cover":"common/21.jpg"},' +
+'{"cover":"common/7.jpg"},' +
+'{"cover":"common/3.jpg"},' +
+'{"cover":"common/1.jpg"},' +
+'{"cover":"common/24.jpg"},' +
+'{"cover":"common/8.jpg"},' +
+'{"cover":"common/4.jpg"},' +
+'{"cover":"common/6.jpg"},' +
+'{"cover":"common/10.jpg"},' +
+'{"cover":"common/23.jpg"},' +
+'{"cover":"common/19.jpg"},' +
+'{"cover":"common/14.jpg"},' +
+'{"cover":"common/9.jpg"},' +
+'{"cover":"common/13.jpg"},' +
+'{"cover":"common/22.jpg"},' +
+'{"cover":"common/20.jpg"},' +
+'{"cover":"common/17.jpg"},' +
+'{"cover":"common/16.jpg"},' +
+'{"cover":"common/11.jpg"},' +
+'{"cover":"common/15.jpg"},' +
+'{"cover":"common/18.jpg"},' +
+'{"cover":"common/5.jpg"}]}';
+
 
 //create the grind
 obj = JSON.parse(tracks);
@@ -61,4 +63,42 @@ for (var i=0; i<24 ; i++){
       
 };
 
-$('.grind').append(html);
+$('.grind').append(html); 
+
+
+$(document).ready(function(){
+
+	//Rotate the refresh icon once 
+	$('.new-songs').click(function(){
+    	var interval = null;
+    	var counter = 0;
+    	var $this = $('.refresh');
+    	clearInterval(interval);
+ 
+   		interval = setInterval(function(){
+        	if (counter != -360) {
+            	counter -= 5;
+           	 	$this.css({
+            	    MozTransform: 'rotate(-' + -counter + 'deg)',
+                	WebkitTransform: 'rotate(' + -counter + 'deg)',
+                	transform: 'rotate(' + -counter + 'deg)'
+            	});
+        	}
+    	}, 10);
+        // Replace with new trakcs
+        obj = JSON.parse(new_tracks);
+
+        html='<div class="row grind">';
+
+        for (var i=0; i<24 ; i++){
+            html += '<div class="element"><img id="track" class="col-md-2 img-thumbnail" alt="" src="'+ obj.tracks[i].cover +'"></div>';
+              
+        };
+        html += '</div>'
+        $('.grind').replaceWith(html); 
+	});	
+
+});
+
+                               
+          
